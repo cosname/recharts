e983916829e2c238bb609f34f99a8246faf140e3
yummyZhou <zhouyanga9@gmail.com>
2013-10-22 23:14:29 +0800

    Update echarts_example.R
    Add two examples for eFroce Function

--- test/echarts_example.R
+++ test/echarts_example.R
@@ -29,3 +29,39 @@ load(url('http://yzhou.org/recharts/ChinaGDP.RData'))
 eMap(ChinaGDP, opt=list(title=list(text='2008~2010年大陆各省GDP占全国百分数')), outfile = 'ChinaGDP')
 
 
+# Force Network
+
+networkMatrix <- matrix(c(
+	c(0, 1, 2, 1, 2, 3, 6, 6, 1, 1, 1 ),
+	c(1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ),
+	c(2, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0 ),
+	c(1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0 ),
+	c(2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 ),
+	c(3, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 ),
+	c(6, 0, 1, 1, 1, 1, 0, 6, 0, 1, 0 ),
+	c(6, 0, 0, 1, 0, 0, 6, 0, 0, 0, 0 ),
+	c(1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ),
+	c(1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 ),
+	c(1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 )
+	), ncol=11
+)
+
+propertyDf <- data.frame(
+	category = c("人物", "家人", "家人", "家人", "家人", "朋友", 
+				"朋友", "朋友", "朋友", "朋友", "朋友"),
+	name = c("Steven Jobs", "Lisa Jobs", "Paul Jobs", " Kalala Jobs",
+			"Lauren Powell", "Steve woz Ike", "Obama", "Bill Gates", 
+			"Jonathan", "Tim Cook", "Wayne"),
+	value = c(10, 2, 3, 3, 7, 5, 8, 9, 4, 4, 0)
+ )
+
+rownames(propertyDf) = propertyDf$name
+
+eForce(networkMatrix=networkMatrix, propertyDf=propertyDf, only=T)
+
+
+# A lite example for Force network
+
+testData <- matrix(1:25, nrow=5)
+eForce(testData, only=T)
+
