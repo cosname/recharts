447b3bc9e87303d0529d516c69ee7b7567e734eb
yummyZhou <zhouyanga9@gmail.com>
2013-10-27 21:09:11 +0800

    shiny demo

--- inst/demo/recharts_shiny.R
+++ inst/demo/recharts_shiny.R
@@ -4,10 +4,3 @@ runApp(system.file("shiny","recharts_shiny_eForce", package = "recharts"))
 pause()
 
 
-
-# shiny Demo first
-library(shiny)
-runApp(system.file("shiny","recharts_shiny_pie", package = "recharts"))
-pause()
-
-
---
+++ inst/demo/recharts_shiny_pie.R
@@ -0,0 +1,7 @@
+
+# shiny Demo first
+library(shiny)
+runApp(system.file("shiny","recharts_shiny_pie", package = "recharts"))
+pause()
+
+
