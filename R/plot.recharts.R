ef09c60fc9db68480c0f29bf42815c557debb0b1
yummyZhou <zhouyanga9@gmail.com>
2013-10-30 17:57:39 +0800

    Local arg added for plot.recharts function
    Local arg added for plot.recharts function for plot Echarts to local.
    Have a check.

--- R/plot.recharts.R
+++ R/plot.recharts.R
@@ -10,9 +10,9 @@
 
 
 
-plot.recharts <- function (x, tag = NULL, ...) 
+plot.recharts <- function (x, tag = NULL, Local = FALSE, ...) 
 {
-	if (missing(tag)) 
+	if (missing(tag))
 		tag <- getOption("recharts.plot.tag")
 	if (is.null(tag) | !("recharts" %in% class(x))) {
 		if (!.isServerRunning()) {
@@ -26,6 +26,7 @@ plot.recharts <- function (x, tag = NULL, ...)
 		if (x$type == "geoVis") {
 			file.copy(getOption("recharts.geoData.dir"), root.dir, recursive = TRUE)
 		}
+
 		if ("recharts" %in% class(x)) {
 			chart.txt <- "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"\n        \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n<html xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <title>%s</title>\n  <meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\" />\n  <style type=\"text/css\">\n    body {\n          color: #444444;\n          font-family: Arial,Helvetica,sans-serif;\n          font-size: 75%%;\n    }\n    a {\n          color: #4D87C7;\n          text-decoration: none;\n    }\n  </style>\n</head>\n<body>\n<p>\n  You find below the HTML code of the visualisation.<br />\n  You can copy and paste the code into an existing HTML page.<br />\n  For more information see also <a href=\"/library/googleVis/html/gvisMethods.html\">?print.recharts</a></p>\n<p><textarea rows=\"50\" name=\"html\" cols=\"80\">\n%s\n</textarea>\n</p>\n</body>\n</html>\n"
 			chart.txt <- sprintf(chart.txt, x$chartid, gsub(">", 
@@ -45,8 +46,18 @@ plot.recharts <- function (x, tag = NULL, ...)
 			file <- file.path(root.dir, basex)
 		}
 		print(x, file = file)
-		.url <- sprintf("http://127.0.0.1:%s/custom/recharts/%s", 
-				tools:::httpdPort, basename(file))
+
+		if (Local){
+			file.copy(file.path(getOption("recharts.template.dir"), "js"), root.dir, recursive = TRUE )
+			localHTML <- readLines(file)
+			localHtml <- gsub("http://efe.baidu.com/echarts/doc/example/www", ".", localHTML)
+			write(localHtml, file = file)
+			#.url <- file.path(root.dir, basename(file))
+			.url <- file
+		}else{
+			.url <- sprintf("http://127.0.0.1:%s/custom/recharts/%s", 
+					tools:::httpdPort, basename(file))
+		}
 		if (interactive()) {
 			browseURL(.url, ...)
 		}
