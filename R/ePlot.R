ec66aa55246135e97eba2b83029a2faa77ac883c
chiffon <chiffonlang@icloud.com>
2017-07-03 17:44:30 +0800

    R-object function and codes

--- R/ePlot.R
+++ R/ePlot.R
@@ -1,78 +1,85 @@
-#' @example
-# series = list(
-# list(
-#   name = "Funnel",
-#   type = "funnel",
-#   size= c('80%', '80%'),
-#   textRotation = c(0, 45, 90, -45),
-#   textPadding = 0,
-#   data = list(list(value=100,
-#                    name="A"),
-#               list(value=66,
-#                    name="B"),
-#               list(value=33,
-#                    name="C"))
-# )
-# )
-# ePlot(series)
-# 
-# series2 = list(
-#   list(
-#     name = '蒸发量',
-#     type = 'bar',
-#     data = c(2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3)
-#   ),
-#   list(
-#     name = '降水量',
-#     type = 'bar',
-#     data = c(2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3)
-#     
-#   ),
-#   list(
-#     name = '平均温度',
-#     type = 'line',
-#     yAxisIndex = 1,
-#     data = c(2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2)
-#     
-#   )
-# )
-# 
-# ext = list(
-#   xAxis = list(list(
-#     type = "category",
-#     data = c('1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月')
-#   )),
-#   yAxis = list(
-#     list(
-#       type =  'value',
-#       name =  '水量',
-#       min =  0,
-#       max =  250,
-#       interval =  50,
-#       axisLabel =  list(
-#         formatter =  '{value} ml'
-#       )
-#     ),
-#     list(
-#       type =  'value',
-#       name =  '温度',
-#       min =  0,
-#       max =  25,
-#       interval =  5,
-#       axisLabel = list(
-#         formatter= '{value} °C'
-#       )
-#     )
-#   ))
-# ePlot(series2, ext )
+#' @title Create rEcharts from an R object
+#' 
+#' @usage 
+#' ePlot (series, ext = NULL, size = NULL)
+#' 
+#' @param series a list object for data in echarts
+#' @param ext xAxis, yAxis, tooltip, toolbox, timeline setting in echarts
+#' @param size an array of html widget width and height(either numeric pixels 
+#'   or percentage could be accepted): e.g. size = c(1024, 768).
+#' @examples
+#' series = list(
+#' list(
+#'   name = "Funnel",
+#'   type = "funnel",
+#'   size= c('80%', '80%'),
+#'   textRotation = c(0, 45, 90, -45),
+#'  textPadding = 0,
+#'   data = list(list(value=100,
+#'                    name="A"),
+#'               list(value=66,
+#'                    name="B"),
+#'               list(value=33,
+#'                    name="C"))
+#' )
+#' )
+#' # ePlot(series)
+#' # 
+#' series2 = list(
+#'   list(
+#'     name = '\u84b8\u53d1\u91cf',
+#'     type = 'bar',
+#'     data = c(2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3)
+#'   ),
+#'   list(
+#'     name = '\u964d\u6c34\u91cf',
+#'     type = 'bar',
+#'     data = c(2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3)
+#' 
+#'   ),
+#'   list(
+#'     name = '\u5e73\u5747\u6e29\u5ea6',
+#'     type = 'line',
+#'     yAxisIndex = 1,
+#'     data = c(2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2)
+#' 
+#'   )
+#' )
+#' 
+#' ext = list(
+#'   xAxis = list(list(
+#'     type = "category",
+#'     data = c('1\u6708','2\u6708','3\u6708','4\u6708','5\u6708','6\u6708','7\u6708','8\u6708','9\u6708','10\u6708','11\u6708','12\u6708')
+#'   )),
+#'   yAxis = list(
+#'     list(
+#'       type =  'value',
+#'       name =  '\u6c34\u91cf',
+#'       min =  0,
+#'       max =  250,
+#'       interval =  50,
+#'       axisLabel =  list(
+#'         formatter =  '{value} ml'
+#'       )
+#'     ),
+#'    list(
+#'       type =  'value',
+#'       name =  '\u6e29\u5ea6',
+#'       min =  0,
+#'       max =  25,
+#'       interval =  5,
+#'       axisLabel = list(
+#'         formatter= '{value} ℃'
+#'       )
+#'     )
+#'   ))
+#' ePlot(series2, ext )
 #' @export
-ePlot = function(series, ext = NULL, size = NULL, fixed = TRUE,
+ePlot = function(series, ext = NULL, size = NULL, 
                  theme = "default", title = NULL, subtitle = NULL, title.x = "center", title.y = "top", legend.data = NULL,
                  legend = TRUE, legend.x = "left", legend.y= "top", legend.orient="horizontal", 
-                 toolbox = TRUE, toolbox.orient = "horizontal", toolbox.x = "right", toolbox.y = "top", 
-                 dataView = TRUE, readOnly = FALSE, mark=TRUE, dataZoom=FALSE, magicType=FALSE,
-                 tooltip = TRUE, tooltip.trigger="item", formatter="", axis.scale=TRUE,
-                 xlab=FALSE, ylab=FALSE, calculable=TRUE, showLabel=TRUE, opt = list())
+                 tooltip = TRUE, tooltip.trigger="item", 
+                 calculable=TRUE,  opt = list())
 {
   if(missing(legend.data) | is.null(legend.data)){legendData = sapply(series, function(x) x[['name']])
   }else{legendData = legend.data}
@@ -91,7 +98,7 @@ ePlot = function(series, ext = NULL, size = NULL, fixed = TRUE,
   
   
  
-  #showLabelLine=showLabel
+
   #now we don't support the multiple graph in one canvas
   opt$series = series
   
