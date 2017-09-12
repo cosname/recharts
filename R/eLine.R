7a865bbac8ee635f7e75ee4c8291dc27fc32867b
chiffon <chiffonlang@icloud.com>
2017-09-12 17:21:24 +0800

    add argument to eLine

--- R/eLine.R
+++ R/eLine.R
@@ -10,6 +10,9 @@
 #' @param series an "formula" object: Associates the levels of variable
 #'   with symbol color, e.g. \code{series = ~groupName}
 #' @param smooth wether the line should be smoothy or not.
+#' @param symbol Icon types provided by ECharts includes 'circle', 'rect', 'roundRect', 'triangle', 'diamond', 'pin', 'arrow'
+#' @param symbolSize symbol size. It can be set to single numbers like 10, or use an array to represent width and height. For example, [20, 10] means symbol width is 20, and height is10.
+#' @param z z -level
 #' @param size an array of html widget width and height(either numeric pixels 
 #'   or percentage could be accepted): e.g. size = c(1024, 768).
 #' @param theme an object of theme name. see(\url{http://datatables.net/extensions/index}) for detail.
@@ -85,6 +88,7 @@
 #' 
 
 eLine = function(dat, xvar=NULL, yvar=NULL, series=NULL, size = NULL, horiz = FALSE, smooth = NULL,
+                 symbol = NULL, symbolSize = NULL, z=NULL,
 	theme = "default", title = NULL, subtitle = NULL, title.x = "center", title.y = "top", 
 	legend = TRUE, legend.x = "left", legend.y= "top", legend.orient="horizontal", 
 	toolbox = TRUE, toolbox.orient = "horizontal", toolbox.x = "right", toolbox.y = "top", 
@@ -184,6 +188,18 @@ eLine = function(dat, xvar=NULL, yvar=NULL, series=NULL, size = NULL, horiz = FA
       
         if(!is.null(smooth)){
           opt$series[[i]]$smooth=smooth
+        }
+      
+        if(!is.null(symbol)){
+          opt$series[[i]]$symbol=symbol
+        }
+        
+        if(!is.null(symbolSize)){
+          opt$series[[i]]$symbolSize=symbolSize
+        }
+      
+        if(!is.null(z)){
+          opt$series[[i]]$z=z
         }
     }
   
